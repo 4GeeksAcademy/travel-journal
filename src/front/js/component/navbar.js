84400fd8e9e88f0aa26cb9df49d7718c7ce4be23
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2024-07-11 09:51:29 +0000

    NavBar maquetacion componente

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+		<>
+		<nav className="navbar navbar-light bg-white p-3">
+			<div className="container-fluid d-flex justify-content-between">
+				<img src="https://raw.githubusercontent.com/4GeeksAcademy/travel-journal/main/src/front/img/logotipo.png" alt="logotipo travel journal" />
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<div className="container-user">
+						<span className="name-user">username</span>
+						<img className="img-user ms-3" src="https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg" alt="" />
+					</div>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
 			</div>
 		</nav>
+		</>
+		
 	);
 };
