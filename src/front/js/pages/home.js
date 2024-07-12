522fbf74c7d10e1ebaa0e9ccf0180dbd5c0adc26
Eduardo-correa1 <159133054+Eduardo-correa1@users.noreply.github.com>
2024-07-12 19:34:05 +0000

    pagina home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,12 +1,20 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import CardPost from "../component/cardPost";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
+		<div className="m-5">
+			<h5>nombre del pais</h5>
+			<div className= "d-flex">
+				<CardPost/>
+				<CardPost/>
+				<CardPost/>
+				<CardPost/>
+			</div>
 			
 
 		</div>
