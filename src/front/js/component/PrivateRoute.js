1b40d3a816e4180ab3b6342b519fe38d43a54318
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2024-08-07 11:50:37 +0200

    Add files via upload

---
+++ src/front/js/component/PrivateRoute.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+export const PrivateRoute = ({ children }) => {
+    const token = localStorage.getItem('jwt-token');
+
+    return token ? children : <Navigate to="/login" />;
+};
